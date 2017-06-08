3ea4cccdf35160a122404a4ec57e77e26e39dd6f
Borja Molina <borjamc1997@hotmail.com>
2017-06-08 22:37:14 +0200

    Inicio

---
+++ index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>Cines Catarroja</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<link href='http://fonts.googleapis.com/css?family=Raleway:400,100,200,300,500,600,700,800,900' rel='stylesheet' type='text/css'>
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-panels.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel-noscript.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-desktop.css" />
+		</noscript>
+		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie/v9.css" /><![endif]-->
+	</head>
+	<body class="homepage">
+
+		<!-- Header -->
+		<div id="header">
+			<div class="container">
+
+				<!-- Logo -->
+				<div id="logo">
+					<h1><a>Autonomy</a></h1>
+				</div>
+
+				<!-- Nav -->
+				<nav id="nav">
+					<ul>
+						<li class="active"><a href="index.php">Pagina principal</a></li>
+						<li><a href="peliculas.php">Peliculas</a></li>
+						<li><a href="contacto.php">Contactanos</a></li>
+					</ul>
+				</nav>
+
+			</div>
+		</div>
+
+		<div id="banner">&nbsp;</div>
+
+		<div id="featured">
+			<div class="container">
+				<div class="row">
+
+					<div class="3u">
+						<section>
+							<a href="#" class="image full"><img src="images/pics02.jpg" alt=""></a>
+							<header>
+								<h2>Etiam posuere</h2>
+							</header>
+							<p>Pellentesque viverra vulputate enim. Aliquam erat volutpat. Pellentesque tristique ante. Sed vel tellus.</p>
+						</section>
+					</div>
+
+					<div class="3u">
+						<section>
+							<a href="#" class="image full"><img src="images/pics03.jpg" alt=""></a>
+							<header>
+								<h2>Fusce ultrices</h2>
+							</header>
+							<p>Pellentesque tristique ante. Sed vel tellus. Curabitur sem urna, consequat suscipit mattis placerat, nulla.</p>
+						</section>
+					</div>
+
+					<div class="3u">
+						<section>
+							<a href="#" class="image full"><img src="images/pics04.jpg" alt=""></a>
+							<header>
+								<h2>Donec dictum</h2>
+							</header>
+							<p>Pellentesque viverra vulputate enim. Aliquam erat volutpat. Curabitur sem urna, consequat mattis placerat, nulla.</p>
+						</section>
+					</div>
+
+					<div class="3u">
+						<section>
+							<a href="#" class="image full"><img src="images/pics05.jpg" alt=""></a>
+							<header>
+								<h2>Maecenas luctus</h2>
+							</header>
+							<p>Pellentesque viverra vulputate enim. Aliquam erat volutpat. Curabitur sem urna, consequat mattis placerat, nulla.</p>
+						</section>
+					</div>
+
+				</div>
+			</div>
+		</div>
+		<!-- Copyright -->
+		<div id="copyright">
+			<div class="container">
+				<a>Design:</a>
+			</div>
+		</div>
+
+	</body>
+</html>
